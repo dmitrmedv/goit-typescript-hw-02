98ff566f6d9ab76aef4715f03b38fc50480bc1b1
dmitrmedv <dmitrmedv@gmail.com>
2024-12-29 00:21:00 +0200

    fix

--- src/Components/ImageCard/ImageCard.tsx
+++ src/Components/ImageCard/ImageCard.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import ImageModal from "../ImageModal/ImageModal";
+import { Photo } from "../../types";
 
-const ImageCard = () => {
+const ImageCard: React.FC<Pick<Photo, "urls" | "alt_description">> = ({
+  urls: { small, regular },
+  alt_description,
+}) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const toggleModal = (): void => {
+    setShowModal((prev) => !prev);
+  };
   return (
-    <div>
-      <img src="" alt="" />
-    </div>
+    <>
+      <div>
+        <img src={small} alt={alt_description} onClick={toggleModal} />
+      </div>
+      {showModal && (
+        <ImageModal
+          largeImageURL={regular}
+          toggleModal={toggleModal}
+          alt={alt_description}
+        ></ImageModal>
+      )}
+    </>
   );
 };
 
--- src/Components/ImageGallery/ImageGallery.tsx
+++ src/Components/ImageGallery/ImageGallery.tsx
@@ -1,32 +1,17 @@
-import React, { useState } from "react";
-import ImageModal from "../ImageModal/ImageModal";
+import React from "react";
 import css from "./ImageGallery.module.css";
 import { ImageSearchResponse } from "../../types";
+import ImageCard from "../ImageCard/ImageCard";
 
 const ImageGallery: React.FC<Pick<ImageSearchResponse, "results">> = ({
   results,
 }) => {
-  const [showModal, setShowModal] = useState<boolean>(false);
-
-  const toggleModal = (): void => {
-    setShowModal((prev) => !prev);
-  };
-
   return (
     <ul className={css.gallery}>
-      {results.map(({ id, urls: { small, regular }, alt_description }) => {
+      {results.map(({ id, urls, alt_description }) => {
         return (
           <li key={id} className={css.galleryItem}>
-            <div className={css.imgWrapper}>
-              <img src={small} alt={alt_description} onClick={toggleModal} />
-            </div>
-            {showModal && (
-              <ImageModal
-                largeImageURL={regular}
-                toggleModal={toggleModal}
-                alt={alt_description}
-              ></ImageModal>
-            )}
+            <ImageCard urls={urls} alt_description={alt_description} />
           </li>
         );
       })}
